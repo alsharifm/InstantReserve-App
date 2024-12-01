a2558c95b87139bc42f53e87ae3df31f33be3964
rmiranda <rmirandagomez4149@sdsu.edu>
2024-11-30 18:44:30 -0800

    messing with structure

--- app/src/app/models/reservation.py
+++ app/src/app/models/reservation.py
@@ -1,6 +1,7 @@
 import enum
 from datetime import UTC, datetime
 
+
 from sqlalchemy import Boolean, Column, DateTime, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -8,12 +9,18 @@ from app.core.database import Base
 
 class Reservation(Base):
     __tablename__ = "reservations"
+    __tablename__ = "reservations"
     
     id = Column(Integer, primary_key=True, index=True)
     creation_date = Column(DateTime, default=datetime.now(UTC))
     reservation_time = Column(Integer, nullable=False)
     party_size = Column(Integer, nullable = False)
 
+    user_id = Column(Integer, ForeignKey("users.id"))
+    #business_id = Column(Integer, ForeignKey("businesses.id"))
     
+    user = relationship("User", back_populates="reservations")
+    #business = relationship("Business", back_populates="reservations")
+
     user = relationship("User", back_populates="reservations")
     #business = relationship("Business", back_populates="reservations")
